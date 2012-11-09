ea45eca83854f54e784e078c185bd61e24836b16
Kevin Miller <kjmiller@stanford.edu>
2012-11-08 17:38:16 -0800

    added run_tesseract.sh

---
+++ OCRKevinTake3/run_tesseract.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+x=/afs/ir.stanford.edu/users/k/j/kjmiller
+
+export TESSDATA_PREFIX=$x/cgi-bin/OCRplus/OCRKevinTake3/tesseract-ocr/tesseract-ocr
+$x/local/bin/tesseract $1 $2 $x/cgi-bin/OCRplus/OCRKevinTake3/tesseract-ocr/tessdata/configs/hocr
