3bc16195b4ecb5650e208ae7003e031c08ed45d4
Kevin Miller <kjmiller@stanford.edu>
2012-11-08 16:55:23 -0800

    now has gaussian blurring

--- Rendering/shitty_renderer.py
+++ Rendering/shitty_renderer.py
@@ -1,27 +1,65 @@
-from PIL.Image import open
+import PIL.Image
 import ImageDraw
 import ImageFont
+import ImageFilter
 import sys
 
-def font_file_name = "Lobster 1.4.otf"
+delimiter = "#OppanOCRplusDelimiter#"
+remove_inner_newlines = 1
+font_file_name = "/afs/ir.stanford.edu/users/k/j/kjmiller/cgi-bin/OCRplus/Rendering/Lobster 1.4.otf"
+num_blurs = 25
 
 def draw_text_on_image(I, draw, x_offset, y_offset, font_size, red, green, blue, text):
 	draw.text((x_offset, y_offset), text, (red, green, blue), font = ImageFont.truetype(font_file_name, font_size))
 
-sys_argv = " ".join(sys.argv[1:]).split("#")
-input_image_name = sys_argv[0]
-output_image_name = sys_argv[1]
-for i in range((len(sys_argv) - 2) / 7):
-	x_offsets.append(sys_argv[2 + 7 * i])
-	y_offsets.append(sys_argv[2 + 7 * i + 1])
-	font_sizes.append(sys_argv[2 + 7 * i + 2])
-	reds.append(sys_argv[2 + 7 * i + 3])
-	greens.append(sys_argv[2 + 7 * i + 4])
-	blues.append(sys_argv[2 + 7 * i + 5])
-	texts.append(sys_argv[2 + 7 * i + 6])
+input_image_name = sys.argv[1]
+input_file_name = sys.argv[2]
+output_image_name = sys.argv[3]
 
-I = open(input_image_name)
+print(input_file_name)
+
+input_file = open(input_file_name, "r")
+input_str = ""
+for line in input_file:
+	if remove_inner_newlines:
+		input_str += line.rstrip("\n")
+	else:
+		input_str += line
+
+input_file.close()
+input_str = input_str.rstrip("\n")
+input_strs = input_str.split(delimiter)
+
+x_offsets = []
+y_offsets = []
+lr_x_offsets = []
+lr_y_offsets = []
+font_sizes = []
+reds = []
+greens = []
+blues = []
+texts = []
+for i in range(len(input_strs) / 9):
+	x_offsets.append(int(input_strs[9 * i]))
+	y_offsets.append(int(input_strs[9 * i + 1]))
+	lr_x_offsets.append(int(input_strs[9 * i + 2]))
+	lr_y_offsets.append(int(input_strs[9 * i + 3]))
+	font_sizes.append(int(input_strs[9 * i + 4]))
+	reds.append(int(input_strs[9 * i + 5]))
+	greens.append(int(input_strs[9 * i + 6]))
+	blues.append(int(input_strs[9 * i + 7]))
+	texts.append(input_strs[9 * i + 8])
+
+I = PIL.Image.open(input_image_name)
 draw = ImageDraw.Draw(I)
+for i in range(len(texts)):
+	bbox = (x_offsets[i], y_offsets[i], lr_x_offsets[i], lr_y_offsets[i])
+	patch = I.crop(bbox)
+	for b in range(num_blurs):
+		patch = patch.filter(ImageFilter.BLUR)
+
+	I.paste(patch, bbox)
+
 for i in range(len(texts)):
 	draw_text_on_image(I, draw, x_offsets[i], y_offsets[i], font_sizes[i], reds[i], greens[i], blues[i], texts[i])
 
--- Translation/translate_the_fucking_sentences.py
+++ Translation/translate_the_fucking_sentences.py
@@ -2,24 +2,65 @@ import urllib
 import simplejson
 import sys
 
+chunk_size = 1 #NOTE: DO NOT CHANGE THIS NUMBER!!!
+remove_inner_newlines = 1
+
+delimiter = "#OppanOCRplusDelimiter#"
+
 base_url = "https://www.googleapis.com/language/translate/v2?"
 
-sys_argv = " ".join(sys.argv[1:]).split("#")
-
-key_file_name = sys_argv[0]
-source = sys_argv[1]
-target = sys_argv[2]
-sentences = sys_argv[3:]
+key_file_name = sys.argv[1]
+target = sys.argv[2]
+input_file_name = sys.argv[3]
 
 key_file = open(key_file_name, "r")
 key = key_file.readline().rstrip("\n")
 key_file.close()
 
+parser_output_file = open(input_file_name, "r")
+parser_output_str = ""
+for line in parser_output_file:
+	if remove_inner_newlines:
+		parser_output_str += line.rstrip("\n")
+	else:
+		parser_output_str += line
+
+parser_output_file.close()
+parser_output_str = parser_output_str.rstrip("\n")
+
+parser_outputs = parser_output_str.split(delimiter)
+
+if len(parser_outputs) == 0:
+	print("")
+	exit()
+
+sentences = []
+
+for i in range(len(parser_outputs) / 2):
+	sentences.append(parser_outputs[2 * i + 1].rstrip("\n"))
+
+#print(sentences)
+
 source_tuple_list = []
-if source != "":
-	source_tuple_list = [("source", source)]
+#if source != "":
+#	source_tuple_list = [("source", source)]
 
-url = base_url + urllib.urlencode([("key", key)] + map(lambda sentence: ("q", sentence), sentences) + source_tuple_list + [("target", target)])
+translations = []
+i = 0
+while i * chunk_size < len(sentences):
+	url = base_url + urllib.urlencode([("key", key)] + map(lambda sentence: ("q", sentence), sentences[i * chunk_size:min((i + 1) * chunk_size, len(sentences))]) + source_tuple_list + [("target", target)])
 
-for blob in simplejson.load(urllib.urlopen(url))["data"]["translations"]:
-	print(blob["translatedText"])
+	#print(url)
+
+	thingy = simplejson.load(urllib.urlopen(url))
+	if "data" in thingy and  "translations" in thingy["data"]:
+		for blob in thingy["data"]["translations"]:
+			translations.append(blob["translatedText"])
+	
+	else:
+		translations.append("")
+
+	i += 1
+	#print(translations)
+
+print(delimiter.join(translations).encode('utf8'))
